ec7ba90d4e1a821dfaa034aa91428e386836a486
Zach Bayoff <zrbayoff@gmail.com>
2018-05-15 14:56:17 -0400

    fixing  accessibility and performance

--- app.js
+++ app.js
@@ -3,6 +3,12 @@ const bodyParser = require('body-parser');
 const app = express();
 const port = process.env.PORT || 8080;
 
+app.get('*.js', function (req, res, next) {
+	req.url = req.url + '.gz';
+	res.set('Content-Encoding', 'gzip');
+	next();
+  });
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
 	extended: false
--- webpack.production.config.js
+++ webpack.production.config.js
@@ -1,4 +1,13 @@
 const path = require('path');
+const webpack = require('webpack');
+const CompressionPlugin = require('compression-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const OptimizeCSSAssetsPlugin = require(
+	"optimize-css-assets-webpack-plugin"
+);
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+const cssnano = require("cssnano");
 
 module.exports = {
 	devtool: 'source-map',
@@ -9,16 +18,51 @@ module.exports = {
 		filename: 'bundle.js'
 	},
 	module: {
-		rules: [
-			{
+		rules: [{
 				test: /\.js$/,
 				exclude: /node_modules/,
 				loader: 'babel-loader',
 				query: {
-                    presets: ['env'],
-                    plugins: ["angularjs-annotate"]
+					presets: ['env'],
+					plugins: ["angularjs-annotate"]
 				}
+			},
+			{
+				test: /\.(png|svg|jpg|gif)$/,
+				use: [{
+					loader: 'file-loader'
+				}]
+			},
+			{
+				test: /\.scss$/,
+				use: [{
+					loader: 'style-loader'
+				}, {
+					loader: 'css-loader'
+				}, {
+					loader: 'sass-loader'
+				}]
 			}
 		]
-	}
+	},
+	plugins: [
+		new webpack.DefinePlugin({
+			'process.env': {
+				'NODE_ENV': JSON.stringify('production')
+			}
+		}),
+		new CompressionPlugin({
+			asset: "[path].gz[query]",
+			algorithm: "gzip",
+			test: /\.js$|\.css$|\.html$/,
+			threshold: 10240,
+			minRatio: 0.8
+		}),
+		new MiniCssExtractPlugin({
+			// Options similar to the same options in webpackOptions.output
+			// both options are optional
+			filename: "styles.css",
+			chunkFilename: "[id].css"
+		})
+	],
 };
